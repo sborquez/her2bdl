27452f3c4082e3841b3c840af25e21544ff97a7c
Sebastián Bórquez <sebastian.borquez.g@gmail.com>
2021-05-25 02:13:56 -0400

    fix: docker launching jupyter notebook

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -4,7 +4,7 @@ By pulling and using the container, you accept the terms and conditions of this 
 https://developer.download.nvidia.com/licenses/NVIDIA_Deep_Learning_Container_License.pdf"""
 
 
-echo """Start a Jupyter Notebook server with `jupyter notebook --ip 0.0.0.0 --port 8888 --allow-root`
+echo """Start a Jupyter Notebook server with jupyter notebook --ip 0.0.0.0 --port 8888 --allow-root
 
 To access it, visit http://localhost:8888 on your host machine.
 Ensure the following arguments were added to 'docker run' to expose the Jupyter Notebook server to your host machine:
